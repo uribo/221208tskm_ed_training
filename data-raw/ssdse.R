ab4049ff37ba51d651835dace0c107d890c80627
Shinya Uryu <suika1127@gmail.com>
2022-12-05 17:24:05 +0900

    [add]研修コンテンツ

---
+++ data-raw/ssdse.R
@@ -0,0 +1,24 @@
+library(ssdse)
+library(dplyr)
+
+df_ssdse_b <- 
+  ssdse::read_ssdse_b("https://www.nstac.go.jp/sys/files/SSDSE-B-2022.csv",
+                    lang = "ja")
+
+df_ssdse_b_tiny <- 
+  df_ssdse_b |>
+  select(`年度`, `都道府県`, `人口・世帯`, `自然環境`, `教育`, `家計`) |> 
+  tidyr::unnest(cols = c(`人口・世帯`, `自然環境`, `教育`, `家計`)) |>
+  select(`年度`, `都道府県`, 
+         `総人口`, `総人口（男）`, `総人口（女）`, 
+         `出生数`, `死亡数`,
+         `年平均気温`, `降水量（年間）`,
+         `幼稚園数`, `小学校数`, `中学校数`, `高等学校数`, `短期大学数`, `大学数`,
+         `消費支出（二人以上の世帯）`, `教育費（二人以上の世帯）`)
+
+df_ssdse_b_tiny_shikoku <- 
+  df_ssdse_b_tiny |> 
+  filter(`都道府県` %in% c("徳島県", "香川県", "愛媛県", "高知県"))
+
+df_ssdse_b_tiny_shikoku |> 
+  readr::write_rds(here::here("data-raw/ssdse_b_tiny_shikoku.rds"))
